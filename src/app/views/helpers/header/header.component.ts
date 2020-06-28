cad76b617a5e6d32e1309bfba3ee695b01f247e6
Otlotleng <nkwadioo@gmail.com>
2020-06-28 23:20:56 +0200

    fullscreen fix
    used full creen api from https://developers.google.com/web/fundamentals/native-hardware/fullscreen/

--- src/app/views/helpers/header/header.component.ts
+++ src/app/views/helpers/header/header.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-
+  isFull = 'grow';
   constructor() { }
 
   ngOnInit(): void {
@@ -14,15 +14,36 @@ export class HeaderComponent implements OnInit {
 
   fullScreen() {
     try {
-      const el = document.querySelector(`body`);
-      const scrollToTop = window.setInterval(() => {
-        const pos = window.pageYOffset;
-        if (pos > 0) {
-          window.scrollTo(0, pos - 50); // how far to scroll on each step
-        } else {
-          window.clearInterval(scrollToTop);
+      // const el = document.querySelector(`body`);
+      // const scrollToTop = window.setInterval(() => {
+      //   const pos = window.pageYOffset;
+      //   if (pos > 0) {
+      //     window.scrollTo(0, pos - 50); // how far to scroll on each step
+      //   } else {
+      //     window.clearInterval(scrollToTop);
+      //   }
+      // }, 16);
+      // setTimeout(function(){
+      //     // This hides the address bar:
+      //     window.scrollTo(0, 1);
+      // }, 0);
+
+      // function toggleFullScreen() {
+        let doc: any = window.document;
+        let docEl: any = doc.documentElement;
+
+        const requestFullScreen = docEl.requestFullscreen || docEl.mozRequestFullScreen || docEl.webkitRequestFullScreen || docEl.msRequestFullscreen;
+        const cancelFullScreen = doc.exitFullscreen || doc.mozCancelFullScreen || doc.webkitExitFullscreen || doc.msExitFullscreen;
+
+        if(!doc.fullscreenElement && !doc.mozFullScreenElement && !doc.webkitFullscreenElement && !doc.msFullscreenElement) {
+          this.isFull = 'shrink';
+          requestFullScreen.call(docEl);
         }
-      }, 16);
+        else {
+          this.isFull = 'grow';
+          cancelFullScreen.call(doc);
+        }
+      // }
     } catch {}
   }
 
